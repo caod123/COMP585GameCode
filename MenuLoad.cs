c0acec4ff0eb5f8f60148322a0b24abda4d39b13
Danny Cao <caoxiaohua@gmail.com>
2015-04-19 17:47:26 -0400

    minor changes

--- MenuLoad.cs
+++ MenuLoad.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 public class MenuLoad : MonoBehaviour {
 
+	public GUIText HiScoreText; 			//GUI HiScore
+
+	void Start() {
+		if (!PlayerPrefs.HasKey ("hiscore"))
+			PlayerPrefs.SetInt ("hiscore", 0);
+		HiScoreText.text = "HISCORE: " + PlayerPrefs.GetInt ("hiscore"); 			//Update the GUI Score
+	}
+
 	public void LoadLevelSelector (string level) {
 		Application.LoadLevel (level);
 	}
