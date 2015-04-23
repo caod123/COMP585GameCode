97255e8b0fac3c8b97184d42bf01e405638f15f8
Alex Frankfort <afrankfort47@gmail.com>
2015-04-23 12:33:24 -0400

    edit the life system to show only the amount of lives instead of text to go along with the ship icon

--- SharedValues_Script.cs
+++ SharedValues_Script.cs
@@ -11,6 +11,7 @@
 /// </summary>
 
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class SharedValues_Script : MonoBehaviour 
@@ -66,7 +67,7 @@ public class SharedValues_Script : MonoBehaviour
 	void FixedUpdate ()
 	{
 		scoreText.text = "Score: " + score; 			//Update the GUI Score
-		livesText.text = "Lives: " + lives; //Update the lives
+		livesText.text = "x " + lives; //Update the lives
 		synthesizableText.text = "[Space]: " + Element.getSynthesizableElement (protons);
 
 		//Update the element Text
